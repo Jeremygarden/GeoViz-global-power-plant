64b1b245d26eedea669d225e72ee7de9822789c3
jeremygarden <mingxlee23@gmail.com>
2026-04-04 16:08:02 +0000

    fix: correct power plant CSV URL

--- src/lib/data/loadPlants.ts
+++ src/lib/data/loadPlants.ts
@@ -1,7 +1,7 @@
 import type { Plant } from "../../types";
 import Papa from "papaparse";
 
-const CSV_URL = "https://raw.githubusercontent.com/wri/global-power-plant-database/master/global_power_plant_database.csv";
+const CSV_URL = "https://raw.githubusercontent.com/wri/global-power-plant-database/master/output_database/global_power_plant_database.csv";
 
 type CsvRow = {
   country: string;
